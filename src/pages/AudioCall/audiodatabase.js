d13bddc3feeb2d40ba17d1d9c6692a0f79f7476f
Pratik Sharma <sharma.pratik2016@gmail.com>
2021-11-29 23:10:19 +0530

    finally audio works with other uses as well

--- src/pages/AudioCall/audiodatabase.js
+++ src/pages/AudioCall/audiodatabase.js
@@ -1,5 +1,10 @@
 import { db } from "../../providers/Firebase";
 
+// create an audioroom
+// with an id
+// let all people join with the
+// 
+
 export const getFirebaseDataOnce = async({cell}) => {
     const ref = db.ref(cell);
     return  (await ref.get()).val();
